f8198cab7ce3536ec0336336e86d9b6ca6863ad7
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2017-07-27 12:34:04 -0300

    agregue message service en frontend

---
+++ Frontend/src/app/services/message.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Message } from '../../models/message';
+
+@Injectable()
+export class MessagesService {
+    
+    showMessages(messages: Message[]) {
+        
+    }
+}
