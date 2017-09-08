36c60e8eb410753794d6c720e8bb1e26fc2393ce
domiguel <domiguel@AZSOF01WD>
2017-09-08 14:42:16 +0000

    Email

--- Backend/src/Sofco.Framework/Mail/MailSender.cs
+++ Backend/src/Sofco.Framework/Mail/MailSender.cs
@@ -1,41 +1,52 @@
 ﻿using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Sofco.Framework.Mail
 {
     public class MailSender
     {
-        public static void Send()
+        /// <summary>
+        /// Envía emails a los destinatarios enviados como parametro
+        /// </summary>
+        /// <param name="recipients">Lista de destinatarios separados por ';'</param>
+        /// <param name="subject">Asunto del mail</param>
+        /// <param name="body">cuerpo del mail</param>
+        public static void Send(string recipients, string fromEmail, string fromDisplayName, string subject, string body, string smtpServer, int smtpPort, string domain)
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Diego", "domiguel@sofrecom.com.ar"));
-            message.To.Add(new MailboxAddress("Lucas", "domiguel84@gmail.com"));
-            message.Subject = "Hello World - A mail from ASPNET Core";
+            message.From.Add(new MailboxAddress(fromDisplayName, fromEmail));
+            AddRecipients(message, recipients);
+            message.Subject = subject;
             message.Body = new TextPart("plain")
             {
-                Text = "Hello World - A mail from ASPNET Core"
+                Text = body
             };
 
             using (var client = new SmtpClient())
             {
-                //client.Connect("smtp.gmail.com", 587, false);
-                client.Connect("sistarSMTP.sofrecom.local", 587, false);
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
 
-                
+                client.Connect(smtpServer, smtpPort, false);
+                client.LocalDomain = domain;
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
-                //client.Authenticate("prudentialpea@gmail.com", "prudentialpeaprudentialpea");
-                // Note: since we don't have an OAuth2 token, disable 	// the XOAUTH2 authentication mechanism.     client.Authenticate("anuraj.p@example.com", "password");
+
                 client.Send(message);
                 client.Disconnect(true);
+            }
+        }
 
-                //client.LocalDomain = "some.domain.com";
-                //await client.ConnectAsync("smtp.relay.uri", 25, SecureSocketOptions.None).ConfigureAwait(false);
-                //await client.SendAsync(emailMessage).ConfigureAwait(false);
-                //await client.DisconnectAsync(true).ConfigureAwait(false);
+        private static void AddRecipients(MimeMessage message, string recipients)
+        {
+            string[] recipientsMails = recipients.Split(';');
+            foreach(var email in recipientsMails)
+            {
+                message.To.Add(new MailboxAddress(email, email));
             }
         }
     }
