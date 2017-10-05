ce03bb635629c11c0dd0e94297b9a6e37afc2444
Francisco <frosales@sofrecom.com.ar>
2017-10-05 10:23:37 -0300

    Added Basic Authentication

---
+++ src/Sofco.WebJob/Settings/JobSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace Sofco.WebJob.Settings
+{
+    public class JobSetting
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
--- src/Sofco.WebJob/Startup.cs
+++ src/Sofco.WebJob/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http.Authentication;
 using Hangfire;
 using Sofco.WebJob.Services;
 using Sofco.WebJob.Filters;
-using Sofco.WebJob.Security.Events;
+using Sofco.WebJob.Security;
 
 namespace Sofco.WebJob
 {
@@ -44,29 +40,7 @@ namespace Sofco.WebJob
 
             app.UseMvc();
 
-            app.UseBasicAuthentication(new BasicAuthenticationOptions
-            {
-                Events = new BasicAuthenticationEvents
-                {
-                    OnValidateCredentials = context =>
-                    {
-                        if (context.Username == context.Password)
-                        {
-                            var claims = new[]
-                            {
-                                new Claim(ClaimTypes.NameIdentifier, context.Username)
-                            };
-                            context.Ticket = new AuthenticationTicket(
-                                new ClaimsPrincipal(
-                                    new ClaimsIdentity(claims, context.Options.AuthenticationScheme)),
-                                new AuthenticationProperties(),
-                                context.Options.AuthenticationScheme);
-                        }
-
-                        return Task.FromResult<object>(null);
-                    }
-                }
-            });
+            app.UseBasicAuthentication(WebJobAuthenticationOptions.Config(Configuration["JobSetting:username"], Configuration["JobSetting:password"]));
 
             app.UseHangfireDashboard("/jobs", new DashboardOptions()
             {
