b7ac52c6edd4f2b177fbdc66fb279f2b55afcd4f
Francisco <frosales@sofrecom.com.ar>
2017-10-30 15:34:16 -0300

    Added UnitTests

---
+++ src/Sofco.UnitTest/Services/Jobs/EmployeeEndJobServiceTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using Sofco.Core.Config;
+using Sofco.Core.DAL.Admin;
+using Sofco.Core.DAL.AllocationManagement;
+using Sofco.Core.Mail;
+using Sofco.Model;
+using Sofco.Model.Models.TimeManagement;
+using Sofco.Service.Implementations.Jobs;
+using Sofco.Service.Settings.Jobs;
+
+namespace Sofco.UnitTest.Services.Jobs
+{
+    [TestFixture]
+    public class EmployeeEndJobServiceTest
+    {
+        const string MailTo = "mail1@mail.com";
+
+        const string TimeZone = "Argentina Standard Time";
+
+        const string PmoCode = "PMO";
+
+        private EmployeeEndJobService sut;
+
+        private Mock<IEmployeeRepository> employeeRepositoryMock;
+
+        private Mock<IGroupRepository> groupRepositoryMock;
+
+        private Mock<IMailSender> mailSenderMock;
+
+        private Mock<IOptions<JobSetting>> JobSettingOptionMock;
+
+        private Mock<IOptions<EmailConfig>> emailOptionMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            employeeRepositoryMock = new Mock<IEmployeeRepository>();
+            groupRepositoryMock = new Mock<IGroupRepository>();
+            mailSenderMock = new Mock<IMailSender>();
+            JobSettingOptionMock = new Mock<IOptions<JobSetting>>();
+            emailOptionMock = new Mock<IOptions<EmailConfig>>();
+
+            JobSettingOptionMock.SetupGet(s => s.Value).Returns(
+                new JobSetting
+                {
+                    EmployeeEndJob = new EmployeeEndJobSetting
+                    {
+                        DaysFromNow = 1
+                    },
+                    LocalTimeZoneName = TimeZone
+                });
+
+            emailOptionMock.SetupGet(s => s.Value).Returns(
+                new EmailConfig
+                {
+                    PmoCode = PmoCode
+                });
+
+            groupRepositoryMock.Setup(s => s.GetEmail(PmoCode)).Returns(MailTo);
+
+            mailSenderMock.Setup(s => s.Send(It.IsAny<Email>()));
+
+            sut = new EmployeeEndJobService(employeeRepositoryMock.Object,
+                groupRepositoryMock.Object,
+                mailSenderMock.Object,
+                JobSettingOptionMock.Object,
+                emailOptionMock.Object);
+        }
+
+        [Test]
+        public void ShouldPassSendNotification()
+        {
+            employeeRepositoryMock.Setup(s => s.GetByEndDate(It.IsAny<DateTime>()))
+                .Returns(new List<Employee>() { new Employee { Id = 1 } });
+
+            sut.SendNotification();
+
+            employeeRepositoryMock.Verify(s => s.GetByEndDate(It.IsAny<DateTime>()), Times.Once);
+
+            groupRepositoryMock.Verify(s => s.GetEmail(PmoCode), Times.Once);
+
+            mailSenderMock.Verify(s => s.Send(It.IsAny<Email>()), Times.Once);
+        }
+
+        [Test]
+        public void ShouldFailSendNotification()
+        {
+            employeeRepositoryMock.Setup(s => s.GetByEndDate(It.IsAny<DateTime>()))
+                .Returns(new List<Employee>());
+
+            sut.SendNotification();
+
+            employeeRepositoryMock.Verify(s => s.GetByEndDate(It.IsAny<DateTime>()), Times.Once);
+
+            groupRepositoryMock.Verify(s => s.GetEmail(PmoCode), Times.Never);
+
+            mailSenderMock.Verify(s => s.Send(It.IsAny<Email>()), Times.Never);
+        }
+    }
+}
---
+++ src/Sofco.UnitTest/Services/Jobs/EmployeeSyncJobServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using Sofco.Core.DAL.AllocationManagement;
+using Sofco.Repository.Rh.Repositories.Interfaces;
+using Sofco.Service.Implementations.Jobs;
+using Sofco.Domain.Rh.Rhpro;
+using Sofco.Domain.Rh.Tiger;
+using Sofco.Model.Models.TimeManagement;
+
+namespace Sofco.UnitTest.Services.Jobs
+{
+    [TestFixture]
+    public class EmployeeSyncJobServiceTest
+    {
+        private EmployeeSyncJobService sut;
+
+        private Mock<ITigerEmployeeRepository> tigerEmployeeRepositoryMock;
+
+        private Mock<IRhproEmployeeRepository> rhproEmployeeRepositoryMock;
+
+        private Mock<IEmployeeRepository> employeeRepositoryMock;
+
+        private Mock<ILicenseTypeRepository> licenseTypeRepositoryMock;
+
+        private Mock<IEmployeeLicenseRepository> employeeLicenseRepositoryMock;
+
+        private Mock<IMapper> mapperMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            tigerEmployeeRepositoryMock = new Mock<ITigerEmployeeRepository>();
+            rhproEmployeeRepositoryMock = new Mock<IRhproEmployeeRepository>();
+            employeeRepositoryMock = new Mock<IEmployeeRepository>();
+            licenseTypeRepositoryMock = new Mock<ILicenseTypeRepository>();
+            employeeLicenseRepositoryMock = new Mock<IEmployeeLicenseRepository>();
+            mapperMock = new Mock<IMapper>();
+
+            sut = new EmployeeSyncJobService(
+                tigerEmployeeRepositoryMock.Object,
+                rhproEmployeeRepositoryMock.Object,
+                employeeRepositoryMock.Object,
+                licenseTypeRepositoryMock.Object,
+                employeeLicenseRepositoryMock.Object,
+                mapperMock.Object);
+        }
+
+        [Test]
+        public void ShouldPassSyncTest()
+        {
+            rhproEmployeeRepositoryMock.Setup(s => s.GetLicenseTypes())
+                .Returns(new List<RhproLicenseType>());
+            rhproEmployeeRepositoryMock.Setup(s => s.GetEmployeeLicensesWithStartDate(It.IsAny<DateTime>()))
+                .Returns(new List<RhproEmployeeLicense>());
+            employeeLicenseRepositoryMock.Setup(s => s.Delete(It.IsAny<List<EmployeeLicense>>(), It.IsAny<DateTime>()));
+            employeeLicenseRepositoryMock.Setup(s => s.Save(It.IsAny<List<EmployeeLicense>>()));
+            tigerEmployeeRepositoryMock.Setup(s => s.GetWithStartDate(It.IsAny<DateTime>()))
+                .Returns(new List<TigerEmployee>());
+            licenseTypeRepositoryMock.Setup(s => s.Save(It.IsAny<List<LicenseType>>()));
+            employeeRepositoryMock.Setup(s => s.Save(It.IsAny<List<Employee>>()));
+
+            sut.Sync();
+
+            rhproEmployeeRepositoryMock.Verify(s => s.GetLicenseTypes(), Times.Once);
+            rhproEmployeeRepositoryMock.Verify(s => s.GetEmployeeLicensesWithStartDate(It.IsAny<DateTime>()), Times.Once);
+            employeeLicenseRepositoryMock.Verify(s => s.Delete(It.IsAny<List<EmployeeLicense>>(), It.IsAny<DateTime>()), Times.Once);
+            employeeLicenseRepositoryMock.Verify(s => s.Save(It.IsAny<List<EmployeeLicense>>()), Times.Once);
+            tigerEmployeeRepositoryMock.Verify(s => s.GetWithStartDate(It.IsAny<DateTime>()), Times.Once);
+            licenseTypeRepositoryMock.Verify(s => s.Save(It.IsAny<List<LicenseType>>()), Times.Once);
+            employeeRepositoryMock.Verify(s => s.Save(It.IsAny<List<Employee>>()), Times.Once);
+        }
+    }
+}
