bae73f81c59b751d0032eedca4cbd3613c13c6ca
Francisco <frosales@sofrecom.com.ar>
2017-11-03 17:07:13 -0300

    Updated Code length

--- src/Sofco.WebApi/Migrations/20171103194741_UpdatedGroupCode.Designer.cs
+++ src/Sofco.WebApi/Migrations/20171103194741_UpdatedGroupCode.Designer.cs
@@ -10,9 +10,10 @@ using Sofco.Model.Enums.TimeManagement;
 namespace Sofco.WebApi.Migrations
 {
     [DbContext(typeof(SofcoContext))]
-    partial class SofcoContextModelSnapshot : ModelSnapshot
+    [Migration("20171103194741_UpdatedGroupCode")]
+    partial class UpdatedGroupCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasDefaultSchema("app")
