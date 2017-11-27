c94aa7cbe1b988d89a52ffcdd1375d0fef33cd93
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2017-11-27 17:08:01 -0300

    unit of work

--- src/Sofco.DAL/IUnitOfWork.cs
+++ src/Sofco.DAL/IUnitOfWork.cs
@@ -4,12 +4,19 @@ namespace Sofco.DAL
 {
     public interface IUnitOfWork
     {
+        #region Admin
+
         IUserRepository UserRepository { get; }
         IRoleRepository RoleRepository { get; }
         IGroupRepository GroupRepository { get; }
         IModuleRepository ModuleRepository { get; }
         IFunctionalityRepository FunctionalityRepository { get; }
         IUserGroupRepository UserGroupRepository { get; }
+        IMenuRepository MenuRepository { get; }
+        IGlobalSettingRepository GlobalSettingRepository { get; }
+        IRoleFunctionalityRepository RoleFunctionalityRepository { get; }
+
+        #endregion
 
         void Save();
     }
--- src/Sofco.DAL/UnitOfWork.cs
+++ src/Sofco.DAL/UnitOfWork.cs
@@ -7,29 +7,37 @@ namespace Sofco.DAL
     {
         private readonly SofcoContext context;
 
+        #region Admin
+
         private IUserRepository userRepository;
         private IRoleRepository roleRepository;
         private IGroupRepository groupRepository;
         private IModuleRepository moduleRepository;
         private IFunctionalityRepository functionalityRepository;
         private IUserGroupRepository userGroupRepository;
+        private IMenuRepository menuRepository;
+        private IGlobalSettingRepository globalSettingRepository;
+        private IRoleFunctionalityRepository roleFunctionalityRepository;
+
+        #endregion
 
         public UnitOfWork(SofcoContext context)
         {
             this.context = context;
         }
 
+        #region Admin
         public IUserRepository UserRepository => userRepository ?? (userRepository = new UserRepository(context));
-
         public IRoleRepository RoleRepository => roleRepository ?? (roleRepository = new RoleRepository(context));
-
         public IGroupRepository GroupRepository => groupRepository ?? (groupRepository = new GroupRepository(context));
-
         public IModuleRepository ModuleRepository => moduleRepository ?? (moduleRepository = new ModuleRepository(context));
-
         public IFunctionalityRepository FunctionalityRepository => functionalityRepository ?? (functionalityRepository = new FunctionalityRepository(context));
-
         public IUserGroupRepository UserGroupRepository => userGroupRepository ?? (userGroupRepository = new UserGroupRepository(context));
+        public IMenuRepository MenuRepository => menuRepository ?? (menuRepository = new MenuRepository(context));
+        public IGlobalSettingRepository GlobalSettingRepository => globalSettingRepository ?? (globalSettingRepository = new GlobalSettingRepository(context));
+        public IRoleFunctionalityRepository RoleFunctionalityRepository => roleFunctionalityRepository ?? (roleFunctionalityRepository = new RoleFunctionalityRepository(context));
+
+        #endregion
 
         public void Save()
         {
