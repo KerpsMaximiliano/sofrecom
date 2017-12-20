6a405414d78cb6f277c221aecd4b19dabe6f735a
Lucas Elian Drughieri <LEDrughieri@sofrecom.com.ar>
2017-12-20 10:12:43 -0300

    pdf viewer en el detalle del remito

--- src/Sofco.ClientWeb/src/app/views/billing/invoice/detail/invoice-detail.component.ts
+++ src/Sofco.ClientWeb/src/app/views/billing/invoice/detail/invoice-detail.component.ts
@@ -167,9 +167,9 @@ export class InvoiceDetailComponent implements OnInit, OnDestroy {
         },
         err => this.errorHandlerService.handleErrors(err));
     }
-
+ 
     exportPdf(){
-        this.service.getPdf(this.model.id).subscribe(file => {
+        this.service.downloadPdf(this.model.id).subscribe(file => {
             FileSaver.saveAs(file, this.model.pdfFileName);
         },
         err => this.errorHandlerService.handleErrors(err));
