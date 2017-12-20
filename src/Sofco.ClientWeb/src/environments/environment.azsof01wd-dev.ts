45416e1e3205023f03abb36dcb2a703c60fa5c89
Francisco <frosales@sofrecom.com.ar>
2017-12-20 09:49:38 -0300

    Updated Environment setting

--- src/Sofco.ClientWeb/src/environments/environment.azsof01wd-dev.ts
+++ src/Sofco.ClientWeb/src/environments/environment.azsof01wd-dev.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    production: true,
-    urlApi:  "http://azsof01wd:8001/api"
-  };
+  production: true,
+  urlApi:  "http://azsof01wd:8001/api",
+  crmCloseStatusCode: "717620004"
+};
   
