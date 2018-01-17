8e7b56c39dd6be92c49c7336cb4f6eddc5ce5eaf
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-01-17 09:42:03 -0300

    fix decimal

--- src/Sofco.WebApi/Migrations/20180117123750_SolfacQuantityToDecimal.Designer.cs
+++ src/Sofco.WebApi/Migrations/20180117123750_SolfacQuantityToDecimal.Designer.cs
@@ -10,9 +10,10 @@ using Sofco.Model.Enums.TimeManagement;
 namespace Sofco.WebApi.Migrations
 {
     [DbContext(typeof(SofcoContext))]
-    partial class SofcoContextModelSnapshot : ModelSnapshot
+    [Migration("20180117123750_SolfacQuantityToDecimal")]
+    partial class SolfacQuantityToDecimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasDefaultSchema("app")
---
+++ src/Sofco.WebApi/Migrations/20180117123750_SolfacQuantityToDecimal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sofco.WebApi.Migrations
+{
+    public partial class SolfacQuantityToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Quantity",
+                schema: "app",
+                table: "HitoDetails",
+                nullable: false,
+                oldClrType: typeof(short));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "Quantity",
+                schema: "app",
+                table: "HitoDetails",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
