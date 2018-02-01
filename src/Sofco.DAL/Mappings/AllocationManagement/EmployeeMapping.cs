ac183e1dce969006b0c332a949d5fe8dd32d1124
Francisco <frosales@sofrecom.com.ar>
2018-02-01 16:31:25 -0300

    Added EmployeeNumber unique

--- src/Sofco.DAL/Mappings/AllocationManagement/EmployeeMapping.cs
+++ src/Sofco.DAL/Mappings/AllocationManagement/EmployeeMapping.cs
@@ -8,11 +8,11 @@ namespace Sofco.DAL.Mappings.AllocationManagement
         public static void MapEmployee(this ModelBuilder builder)
         {
             builder.Entity<Employee>().HasKey(_ => _.Id);
+            builder.Entity<Employee>().HasIndex(u => u.EmployeeNumber).IsUnique();
             builder.Entity<Employee>().Property(x => x.Name).HasMaxLength(100);
             builder.Entity<Employee>().Property(x => x.Profile).HasMaxLength(100);
             builder.Entity<Employee>().Property(x => x.Seniority).HasMaxLength(100);
             builder.Entity<Employee>().Property(x => x.Technology).HasMaxLength(300);
-            builder.Entity<Employee>().Property(x => x.EmployeeNumber).HasMaxLength(50);
             builder.Entity<Employee>().Property(x => x.CreatedByUser).HasMaxLength(50);
         }
     }
