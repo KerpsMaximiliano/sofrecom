021ba110d0853b44037274c47267fb0864804219
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-04-12 16:50:06 -0300

    fixes

--- src/Sofco.ClientWeb/src/app/components/select2/select2.component.ts
+++ src/Sofco.ClientWeb/src/app/components/select2/select2.component.ts
@@ -22,14 +22,22 @@ export class Select2Component implements OnInit{
     ngOnInit(): void {
         var self = this;
         $(this.element.nativeElement).on('change', function() { 
-            self.value = self.element.nativeElement.value;
-            self.valueChange.emit(self.value);
+            if(self.element.nativeElement.value && self.element.nativeElement.value != undefined){
+                self.value = self.element.nativeElement.value;
+                self.valueChange.emit(self.value);
+            }
         });
     }
 
     ngOnChanges(changes: any) {
-        if(changes.value && changes.value.currentValue != undefined){
+        if(changes.value && changes.value.currentValue != undefined && this.options.length > 0){
             $(this.element.nativeElement).val(changes.value.currentValue).trigger('change');
+        }
+
+        if(changes.options && changes.options.currentValue.length > 0){
+            setTimeout(() => {
+                $(this.element.nativeElement).val(this.value).trigger('change');
+            }, 0);
         }
     }
 }
