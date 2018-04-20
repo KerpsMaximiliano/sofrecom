6360ae335f562dac1632706101d2758897e05b5d
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-04-20 10:44:43 -0300

    integracion resumen calendario

---
+++ src/Sofco.Core/Models/WorkTimeManagement/WorkTimeResumeModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Sofco.Core.Models.WorkTimeManagement
+{
+    public class WorkTimeResumeModel
+    {
+        public int HoursApproved { get; set; }
+
+        public int HoursRejected { get; set; }
+
+        public int HoursPending { get; set; }
+
+        public int HoursPendingApproved { get; set; }
+
+        public int Total => HoursApproved + HoursRejected + HoursPending + HoursPendingApproved;
+
+        public int BusinessHours { get; set; }
+
+        public int HoursUntilToday { get; set; }
+    }
+}
