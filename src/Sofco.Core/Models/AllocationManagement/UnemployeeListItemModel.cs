5d63caa9f1696ed59f07c408b4117cdc5769dcc9
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-04-25 17:26:30 -0300

    be vista empleados dados de baja

---
+++ src/Sofco.Core/Models/AllocationManagement/UnemployeeListItemModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sofco.Model;
+
+namespace Sofco.Core.Models.AllocationManagement
+{
+    public class UnemployeeListItemModel : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string EndReasonType { get; set; }
+
+        public string EndReasonComments { get; set; }
+    }
+}
