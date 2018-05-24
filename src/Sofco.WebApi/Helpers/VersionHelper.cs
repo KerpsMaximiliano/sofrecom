59c6df6c180208704368e8b1152a63fb34e60809
Francisco <frosales@sofrecom.com.ar>
2018-05-24 15:16:55 -0300

    Fixed CRM Hitos Update

--- src/Sofco.WebApi/Helpers/VersionHelper.cs
+++ src/Sofco.WebApi/Helpers/VersionHelper.cs
@@ -6,7 +6,7 @@
 
         private const int Minor = 28;
 
-        private const int Revision = 147;
+        private const int Revision = 148;
 
         private const string AppVersionFormat = "{0}.{1}.{2}";
 
