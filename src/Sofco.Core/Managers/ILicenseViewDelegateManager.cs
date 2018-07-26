311a933b3a7fe865d05445bdc52bd5106dec424b
Francisco <frosales@sofrecom.com.ar>
2018-07-26 17:03:51 -0300

    Add View delegate role in Menu

---
+++ src/Sofco.Core/Managers/ILicenseViewDelegateManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Sofco.Model.Models.Admin;
+
+namespace Sofco.Core.Managers
+{
+    public interface ILicenseViewDelegateManager
+    {
+        List<Role> GetDelegatedRoles();
+    }
+}
