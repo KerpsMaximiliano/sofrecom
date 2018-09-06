e9b4594c2c4ec407feae843d8e66c963805fa8bc
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-09-06 12:15:19 -0300

    fix get custormers and services for daf

--- src/Sofco.ClientWeb/src/app/views/billing/purchaseOrder/edit/edit-purchaseOrder.component.ts
+++ src/Sofco.ClientWeb/src/app/views/billing/purchaseOrder/edit/edit-purchaseOrder.component.ts
@@ -81,7 +81,6 @@ export class EditPurchaseOrderComponent implements OnInit, OnDestroy {
 
                 setTimeout(() => {
                     $('#analytics').val(this.form.model.analyticIds).trigger('change');
-                    this.form.searchOpportunities();
                 }, 1000);
 
                 if(this.form.model.status != PurchaseOrderStatus.Draft 
