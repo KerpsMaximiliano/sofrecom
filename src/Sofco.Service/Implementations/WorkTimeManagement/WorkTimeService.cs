283bf7bd1b70efd59e19aca9f4f3a035239fd51b
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-09-27 17:28:42 -0300

    unit test

--- src/Sofco.Service/Implementations/WorkTimeManagement/WorkTimeService.cs
+++ src/Sofco.Service/Implementations/WorkTimeManagement/WorkTimeService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Sofco.Core.Data.Admin;
 using Sofco.Core.DAL;
