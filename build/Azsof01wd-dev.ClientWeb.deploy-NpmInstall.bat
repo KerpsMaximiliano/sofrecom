3f2f9140f24020b002e6d85806bf0eed275e746d
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-10-22 11:52:09 -0300

    fix jenkins

--- build/Azsof01wd-dev.ClientWeb.deploy-NpmInstall.bat
+++ build/Azsof01wd-dev.ClientWeb.deploy-NpmInstall.bat
@@ -20,4 +20,7 @@ cmd /c ng build --prod --configuration=azsof01wd-dev --output-path "%PUBLISH_PAT
 
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
+cmd /c DEL /F /Q "%DEPLOY_PATH%"
+cmd /c FOR /D %%p IN ("%DEPLOY_PATH%\*.*") DO rmdir "%%p" /s /q
+
 cmd /c XCOPY /S "%PUBLISH_PATH%" "%DEPLOY_PATH%"
