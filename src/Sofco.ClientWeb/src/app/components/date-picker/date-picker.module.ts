5db3dd57bda12f4c3109a2deb3c5bdc1f4433bd1
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-10-25 16:59:48 -0300

    cambio de datepicker

--- src/Sofco.ClientWeb/src/app/components/date-picker/date-picker.module.ts
+++ src/Sofco.ClientWeb/src/app/components/date-picker/date-picker.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DatePickerComponent } from './date-picker.component';
 import { FormsModule } from '@angular/forms';
 import { DatepickerDirectiveModule } from './date-picker.directive';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, DatepickerDirectiveModule
+    CommonModule, FormsModule, DatepickerDirectiveModule, BsDatepickerModule
   ],
   declarations: [ 
     DatePickerComponent
