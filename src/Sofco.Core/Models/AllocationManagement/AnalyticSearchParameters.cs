db97b5ee186ac37bae3554558fcae5cdd623ceee
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-11-12 12:58:51 -0300

    fix ticket 7927

--- src/Sofco.Core/Models/AllocationManagement/AnalyticSearchParameters.cs
+++ src/Sofco.Core/Models/AllocationManagement/AnalyticSearchParameters.cs
@@ -2,14 +2,14 @@
 {
     public class AnalyticSearchParameters
     {
-        public int AnalyticId { get; set; }
+        public int? AnalyticId { get; set; }
 
         public string CustomerId { get; set; }
 
         public string ServiceId { get; set; }
 
-        public int AnalyticStatusId { get; set; }
+        public int? AnalyticStatusId { get; set; }
 
-        public int ManagerId { get; set; }
+        public int? ManagerId { get; set; }
     }
 }
