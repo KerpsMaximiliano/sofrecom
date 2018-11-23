19ba992f96cb4277fa842ee273817979e54fb44a
Francisco <frosales@sofrecom.com.ar>
2018-11-23 14:53:18 -0300

    Fixed EmployeeEndNotifiaction Applicant Excel

--- src/Sofco.ClientWeb/src/app/views/human-resources/end-notification/end-notification.component.ts
+++ src/Sofco.ClientWeb/src/app/views/human-resources/end-notification/end-notification.component.ts
@@ -159,8 +159,8 @@ export class EndNotificationComponent implements OnInit, OnDestroy {
 
     customizeExcelExportData(data) {
         const idIndex = 0;
-        const endDateIndex = 2;
-        const createdDateIndex = 3;
+        const endDateIndex = 3;
+        const createdDateIndex = 4;
         data.header.splice(0, 1);
         const dataBody = data.body;
         for(let index = 0; index < dataBody.length; index++) {
