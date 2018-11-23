fe1942e5103f4882b29911a5e06ccfbe1898a501
Francisco <frosales@sofrecom.com.ar>
2018-11-23 14:29:58 -0300

    Added EmployeeEndNotification roles

--- etc/Sqls/32.EmployeeEndNotifView.sql
+++ etc/Sqls/32.EmployeeEndNotifView.sql
@@ -7,10 +7,31 @@ BEGIN
 END
 
 DECLARE @FuncId Int = (SELECT Id FROM app.Functionalities WHERE Code = 'EMPLOYEE_END_NOTIF')
-DECLARE @RoleId Int = (SELECT Id from app.Roles WHERE Description = 'RRHH AB')
+DECLARE @RrhhRoleId Int = (SELECT Id from app.Roles WHERE Description = 'RRHH')
+DECLARE @MangerRoleId Int = (SELECT Id from app.Roles WHERE Description = 'Gerente')
+DECLARE @DirectorRoleId Int = (SELECT Id from app.Roles WHERE Description = 'Director')
+DECLARE @PmoRoleId Int = (SELECT Id from app.Roles WHERE Description = 'PMO')
 
-IF (NOT EXISTS (SELECT 1 FROM app.RoleFunctionality WHERE RoleId = @RoleId AND FunctionalityId = @FuncId))
+IF (NOT EXISTS (SELECT 1 FROM app.RoleFunctionality WHERE RoleId = @RrhhRoleId AND FunctionalityId = @FuncId))
 BEGIN
 	INSERT INTO app.RoleFunctionality  (RoleId, FunctionalityId) 
-	VALUES (@RoleId, @FuncId);
+	VALUES (@RrhhRoleId, @FuncId);
+END
+
+IF (NOT EXISTS (SELECT 1 FROM app.RoleFunctionality WHERE RoleId = @MangerRoleId AND FunctionalityId = @FuncId))
+BEGIN
+	INSERT INTO app.RoleFunctionality  (RoleId, FunctionalityId) 
+	VALUES (@MangerRoleId, @FuncId);
+END
+
+IF (NOT EXISTS (SELECT 1 FROM app.RoleFunctionality WHERE RoleId = @DirectorRoleId AND FunctionalityId = @FuncId))
+BEGIN
+	INSERT INTO app.RoleFunctionality  (RoleId, FunctionalityId) 
+	VALUES (@DirectorRoleId, @FuncId);
+END
+
+IF (NOT EXISTS (SELECT 1 FROM app.RoleFunctionality WHERE RoleId = @PmoRoleId AND FunctionalityId = @FuncId))
+BEGIN
+	INSERT INTO app.RoleFunctionality  (RoleId, FunctionalityId) 
+	VALUES (@PmoRoleId, @FuncId);
 END
