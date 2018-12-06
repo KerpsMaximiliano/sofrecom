f89897664b673b1fa90347bd33bf52610fe9f737
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-12-06 15:44:15 -0300

    configuracion viaticos

--- src/Sofco.ClientWeb/src/environments/environment.azgap01wp.ts
+++ src/Sofco.ClientWeb/src/environments/environment.azgap01wp.ts
@@ -3,6 +3,7 @@ export const environment = {
   urlApi:  'https://gaps.sofrecom.com.ar/api',
   crmCloseStatusCode: '979730000',
   advacementWorkflowId: 2,
+  viaticumWorkflowId: 3,
   draftWorkflowStateId: 8,
   rejectedWorkflowStateId: 2
 };
