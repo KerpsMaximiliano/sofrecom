0c9a9a3ebe0e9fdb38ebf89354cc0d7655320946
Francisco <frosales@sofrecom.com.ar>
2018-12-10 16:44:15 -0300

    Added CrmService GetAll

---
+++ src/Sofco.Service.Crm/Interfaces/ICrmServiceService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Sofco.Domain.Crm.Billing;
+
+namespace Sofco.Service.Crm.Interfaces
+{
+    public interface ICrmServiceService
+    {
+        List<CrmService> GetAll();
+    }
+}
--- src/Sofco.Service/Implementations/Jobs/ServiceUpdateJobService.cs
+++ src/Sofco.Service/Implementations/Jobs/ServiceUpdateJobService.cs
@@ -1,36 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.Options;
-using Sofco.Core.Config;
 using Sofco.Core.Data.Billing;
 using Sofco.Core.DAL;
 using Sofco.Core.Logger;
 using Sofco.Core.Services.Jobs;
 using Sofco.Domain.Crm.Billing;
-using Sofco.Service.Http.Interfaces;
+using Sofco.Service.Crm.Interfaces;
 
 namespace Sofco.Service.Implementations.Jobs
 {
     public class ServiceUpdateJobService : IServiceUpdateJobService
     {
         private readonly IUnitOfWork unitOfWork;
-        private readonly ICrmHttpClient client;
-        private readonly CrmConfig crmConfig;
         private readonly ILogMailer<ServiceUpdateJobService> logger;
         private readonly IServiceData serviceData;
+        private readonly ICrmServiceService crmServiceService;
 
-        private IList<int> IdsAdded { get; set; }
+        private IList<int> IdsAdded { get; }
 
         public ServiceUpdateJobService(IUnitOfWork unitOfWork,
-            ICrmHttpClient client,
             IServiceData serviceData,
-            IOptions<CrmConfig> crmOptions,
-            ILogMailer<ServiceUpdateJobService> logger)
+            ILogMailer<ServiceUpdateJobService> logger, 
+            ICrmServiceService crmServiceService)
         {
             this.unitOfWork = unitOfWork;
-            this.client = client;
-            crmConfig = crmOptions.Value;
             this.logger = logger;
+            this.crmServiceService = crmServiceService;
             this.serviceData = serviceData;
 
             IdsAdded = new List<int>();
@@ -38,13 +33,11 @@ namespace Sofco.Service.Implementations.Jobs
 
         public void Execute()
         {
-            var url = $"{crmConfig.Url}/api/service";
-
-            var result = client.GetMany<CrmService>(url);
+            var result = crmServiceService.GetAll();
 
             unitOfWork.BeginTransaction();
 
-            foreach (var crmService in result.Data)
+            foreach (var crmService in result)
             {
                 var service = unitOfWork.ServiceRepository.GetByIdCrm(crmService.Id);
 
@@ -80,7 +73,7 @@ namespace Sofco.Service.Implementations.Jobs
             }
             catch (Exception e)
             {
-                logger.LogError($"Error modificacion servicio: {crmService.Nombre}", e);
+                logger.LogError($"Error on update Service: {crmService.Name}", e);
             }
         }
 
@@ -97,14 +90,14 @@ namespace Sofco.Service.Implementations.Jobs
             }
             catch (Exception e)
             {
-                logger.LogError($"Error alta servicio: {crmService.Nombre}", e);
+                logger.LogError($"Error on insert Service: {crmService.Name}", e);
             }
         }
 
         private void FillData(Domain.Models.Billing.Service service, CrmService crmService)
         {
             service.CrmId = crmService.Id;
-            service.Name = crmService.Nombre;
+            service.Name = crmService.Name;
             service.AccountId = crmService.AccountId;
             service.AccountName = crmService.AccountName;
             service.Industry = crmService.Industry;
