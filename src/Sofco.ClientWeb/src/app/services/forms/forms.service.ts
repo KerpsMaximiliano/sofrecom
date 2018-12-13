3281aa6189550890500b997941a1aa4a31268ec6
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2018-12-13 13:37:15 -0300

    refactor

---
+++ src/Sofco.ClientWeb/src/app/services/forms/forms.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class FormsService {
+
+    constructor() {}
+
+    getClassProperty(form, property){
+        if(!form || !form.controls[property]) return;
+
+        if(form.controls[property].invalid && (form.controls[property].dirty || form.controls[property].touched)) return 'has-error';
+        if(form.controls[property].valid && (form.controls[property].dirty || form.controls[property].touched)) return 'has-success';
+    }
+
+    hasErrors(form, property){
+        if(!form || !form.controls[property]) return;
+
+        if(form.controls[property].invalid && (form.controls[property].dirty || form.controls[property].touched)){
+            return form.controls[property].errors;
+        }
+
+        return false;
+    }
+
+    hasError(form, property, validation){
+        if(!form || !form.controls[property]) return;
+
+        return form.controls[property].errors[validation];
+    }
+
+    canSave(form){
+        if(form.valid) return true;
+        
+        return false;
+    }
+}
