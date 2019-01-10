d9c8bb1202fdde95d794fa489b74d4937d908b41
Francisco <frosales@sofrecom.com.ar>
2019-01-10 16:38:17 -0300

    Improved WorkTime Performance by Holidays & Setting cache data

---
+++ src/Sofco.Core/Data/WorktimeManagement/IHolidayData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Sofco.Domain.Models.WorkTimeManagement;
+
+namespace Sofco.Core.Data.WorktimeManagement
+{
+    public interface IHolidayData
+    {
+        List<Holiday> Get(int year, int month);
+    }
+}
