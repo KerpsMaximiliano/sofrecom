f725cee18990bdec37891182d290e5397d81bd20
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-03-08 16:57:58 -0300

    merge

--- src/Sofco.ClientWeb/src/environments/environment.azgap01wp.ts
+++ src/Sofco.ClientWeb/src/environments/environment.azgap01wp.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  urlApi:  'https://gaps.sofrecom.com.ar/api',
-  // urlApi:  'http://azgap01wp-web.azurewebsites.net/api',
+  // urlApi:  'https://gaps.sofrecom.com.ar/api',
+  urlApi:  'http://azgap01wp-web.azurewebsites.net/api',
   crmCloseStatusCode: '979730000',
   draftWorkflowStateId: 8,
   currencyPesosId: 1,
