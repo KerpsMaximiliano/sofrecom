044760e4ee95857d6b9660785fa17d8373516bbc
Emmanuel Pompolo <EPompolo@proveedor.personal.com.ar>
2019-03-18 09:49:31 -0300

    Se corrige exportacion a excel de adelantos

--- src/Sofco.ClientWeb/src/app/views/advancementAndRefund/advancement/list-in-process/list-in-process.component.ts
+++ src/Sofco.ClientWeb/src/app/views/advancementAndRefund/advancement/list-in-process/list-in-process.component.ts
@@ -48,7 +48,7 @@ export class AdvancementListInProcessComponent implements OnInit, OnDestroy {
     }
 
     initGrid() {
-        var columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        var columns = [0, 1, 2, 3, 4, 5, 6];
         var title = `Adelantos-en-proceso`;
 
         var params = {
