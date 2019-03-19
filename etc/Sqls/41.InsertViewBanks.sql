d0076799a4d0cf53625b56d0972cee8218985f01
Emmanuel Pompolo <EPompolo@proveedor.personal.com.ar>
2019-03-19 11:57:28 -0300

    Se corrige Ticket 9025:Adelantos y Reintegros - Pendiente de deposito - parametros de busqueda- mal tipo adelanto

--- etc/Sqls/41.InsertViewBanks.sql
+++ etc/Sqls/41.InsertViewBanks.sql
@@ -1,11 +1,11 @@
+
 CREATE VIEW [report].[BanksView] AS
 
 	SELECT DISTINCT(e.Bank) as 'Name'
 		 ,CAST(ROW_NUMBER() OVER (ORDER BY e.Bank) AS INT) AS 'Id'
 	FROM app.Employees e
-	WHERE e.Bank IS NOT    NULL
-		AND     LTRIM(RTRIM(e.Bank)) != ''
+	WHERE e.Bank IS NOT NULL
+		AND LTRIM(RTRIM(e.Bank)) != ''
 	GROUP BY e.bank
-
 
 GO
