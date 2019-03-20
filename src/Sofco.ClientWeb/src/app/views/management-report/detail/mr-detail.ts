278a750b2db8ff6047c123c5947db5193ab89922
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-03-20 16:08:34 -0300

    inicio informe

--- src/Sofco.ClientWeb/src/app/views/management-report/detail/mr-detail.ts
+++ src/Sofco.ClientWeb/src/app/views/management-report/detail/mr-detail.ts
@@ -29,4 +29,24 @@ export class ManagementReportDetailComponent implements OnInit, OnDestroy {
     ngOnDestroy(): void {
         if(this.paramsSubscrip) this.paramsSubscrip.unsubscribe();
     }
+
+    collapse() {
+        if ($("#collapseOne").hasClass('in')) {
+            $("#collapseOne").removeClass('in');
+        }
+        else {
+            $("#collapseOne").addClass('in');
+        }
+
+        this.changeIcon();
+    }
+
+    changeIcon() {
+        if ($("#collapseOne").hasClass('in')) {
+            $("#search-icon").toggleClass('fa-caret-down').toggleClass('fa-caret-up');
+        }
+        else {
+            $("#search-icon").toggleClass('fa-caret-up').toggleClass('fa-caret-down');
+        }
+    }
 }
