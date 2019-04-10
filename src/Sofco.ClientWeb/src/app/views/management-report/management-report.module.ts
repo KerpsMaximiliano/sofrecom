dfc1072d11d5369528e1ad4674a148a6c5314ab0
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-04-10 11:00:04 -0300

    fix T-9280

--- src/Sofco.ClientWeb/src/app/views/management-report/management-report.module.ts
+++ src/Sofco.ClientWeb/src/app/views/management-report/management-report.module.ts
@@ -22,6 +22,7 @@ import { UtilsService } from "app/services/common/utils.service";
 import { MessageService } from "app/services/common/message.service";
 import { DecimalFormatModule } from "app/components/decimalFormat/decimal-format.directive";
 import { CostDetailMonthComponent } from "./cost-detail-month/cost-detail-month";
+import { DigitModule } from "app/components/digit-limit/digit-limit.directive";
 
 @NgModule({
     declarations: [
@@ -40,6 +41,7 @@ import { CostDetailMonthComponent } from "./cost-detail-month/cost-detail-month"
       Ng2ModalModule, 
       AmountFormatModule,
       PCheckModule,
+      DigitModule,
       NgSelectModule
     ],
     providers: [ ManagementReportService, DatesService, MenuService, UtilsService, ProjectService, MessageService ],
