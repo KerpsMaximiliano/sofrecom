319876c0f3092b96f233e894673d169805283784
Emmanuel Pompolo <EPompolo@proveedor.personal.com.ar>
2019-05-03 16:35:40 -0300

    Se agregan script que no estaban creados

--- etc/Sqls/42.InsertDetailCostFuncionalities.sql
+++ etc/Sqls/42.InsertDetailCostFuncionalities.sql
@@ -1,4 +1,15 @@
+
+INSERT INTO [app].[Modules]
+           ([Active], [Code] ,[Description])
+     VALUES
+           (1, N'MANRE', N'Informe de Gestión')
+
 DECLARE @IdAdvan INT = (SELECT m.Id FROM app.Modules m WHERE	m.Code = 'MANRE') 
+
+INSERT INTO [app].[Functionalities]
+           ([Active], [Code], [Description], [ModuleId])
+     VALUES
+           (1, N'VIEW-DETAIL', N'Vista detalle', @IdAdvan)
 
 INSERT INTO [app].[Functionalities]
            ([Active], [Code], [Description], [ModuleId])
