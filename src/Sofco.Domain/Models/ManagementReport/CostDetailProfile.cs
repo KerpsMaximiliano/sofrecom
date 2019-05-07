9707168d09ce0c2b57955840cccc021137eb5ceb
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-05-07 12:01:47 -0300

    estructura informe de gestion

---
+++ src/Sofco.Domain/Models/ManagementReport/CostDetailProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace Sofco.Domain.Models.ManagementReport
+{
+    public class CostDetailProfile : BaseEntity
+    {
+        public int CostDetailId { get; set; }
+
+        public CostDetail CostDetail { get; set; }
+
+        public decimal Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/Sofco.Domain/Models/ManagementReport/CostDetailResource.cs
@@ -0,0 +1,26 @@
+﻿using Sofco.Domain.Models.Admin;
+using Sofco.Domain.Models.AllocationManagement;
+
+namespace Sofco.Domain.Models.ManagementReport
+{
+    public class CostDetailResource : BaseEntity
+    {
+        public int CostDetailId { get; set; }
+
+        public CostDetail CostDetail { get; set; }
+
+        public decimal Value { get; set; }
+
+        public decimal Adjustment { get; set; }
+
+        public decimal Charges { get; set; }
+
+        public int EmployeeId { get; set; }
+        
+        public Employee Employee { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
