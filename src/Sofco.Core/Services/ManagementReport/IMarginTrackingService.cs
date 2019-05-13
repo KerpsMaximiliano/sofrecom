8e878515089b43b0d9693dd506ba73c72b448626
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-05-13 14:33:14 -0300

    seguimiento de margen

---
+++ src/Sofco.Core/Services/ManagementReport/IMarginTrackingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Sofco.Core.Models.ManagementReport;
+using Sofco.Domain.Utils;
+
+namespace Sofco.Core.Services.ManagementReport
+{
+    public interface IMarginTrackingService
+    {
+        Response<IList<MarginTrackingModel>> Get(int managementReportId);
+    }
+}
