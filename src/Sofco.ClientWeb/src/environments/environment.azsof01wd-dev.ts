51d7bccd30f78ebb66894defda901cd7f05a496f
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-05-22 09:26:22 -0300

    fix

--- src/Sofco.ClientWeb/src/environments/environment.azsof01wd-dev.ts
+++ src/Sofco.ClientWeb/src/environments/environment.azsof01wd-dev.ts
@@ -2,8 +2,15 @@ export const environment = {
   production: true,
   urlApi:  'http://azsof01wd:8000/api',
   crmCloseStatusCode: '717620004',
-  draftWorkflowStateId: 8,
   currencyPesosId: 1,
+  draftWorkflowStateId: 8,
   gafWorkflowStateId: 10,
-  rejectedWorkflowStateId: 2
+  dafWorkflowStateId: 4,
+  rrhhWorkflowStateId: 6,
+  rejectedWorkflowStateId: 2,
+  pendingDirectorWorkflowStateId: 12,
+  pendingManagerWorkflowStateId: 9,
+  pendingComplianceWorkflowStateId: 5,
+  pendingComplianceDWorkflowStateId: 22,
+  pendingComplianceDGWorkflowStateId: 23
 };
