db2e2f62aca4fd52434bf88d844294df481ba3e7
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2019-07-29 11:45:42 -0300

    fix osde, swiss file

--- src/Sofco.Service/Implementations/Rrhh/PrepaidFactory.cs
+++ src/Sofco.Service/Implementations/Rrhh/PrepaidFactory.cs
@@ -9,10 +9,12 @@ namespace Sofco.Service.Implementations.Rrhh
     public class PrepaidFactory : IPrepaidFactory
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly ILogMailer<OsdePrepaidFileManager> logger;
 
-        public PrepaidFactory(IUnitOfWork unitOfWork)
+        public PrepaidFactory(IUnitOfWork unitOfWork, ILogMailer<OsdePrepaidFileManager> logger)
         {
             this.unitOfWork = unitOfWork;
+            this.logger = logger;
         }
 
         public IPrepaidFileManager GetInstance(string code)
@@ -21,7 +23,7 @@ namespace Sofco.Service.Implementations.Rrhh
             {
                 case "SWISS": return new SwissPrepaidFileManager(unitOfWork);
                 case "DOCTHOS": return new DocthosPrepaidFileManager(unitOfWork);
-                case "OSDE": return new OsdePrepaidFileManager(unitOfWork);
+                case "OSDE": return new OsdePrepaidFileManager(unitOfWork, logger);
                 case "OMINT": return new OmintPrepaidFileManager(unitOfWork);
                 case "MEDIFE": return new MedifePrepaidFileManager(unitOfWork);
                 case "MEDICUS": return new MedicusPrepaidFileManager(unitOfWork);
