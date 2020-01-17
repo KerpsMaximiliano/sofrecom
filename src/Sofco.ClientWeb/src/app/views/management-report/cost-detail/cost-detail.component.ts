573175bc87aa58a65ee94a1b5b4cc9e02686fad5
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2020-01-17 11:06:41 -0300

    fix

--- src/Sofco.ClientWeb/src/app/views/management-report/cost-detail/cost-detail.component.ts
+++ src/Sofco.ClientWeb/src/app/views/management-report/cost-detail/cost-detail.component.ts
@@ -526,8 +526,8 @@ export class CostDetailComponent implements OnInit, OnDestroy {
             var _charges = 0
             var _total = 0
 
-            if (monthCost.real.id && monthCost.real.id > 0) {
-                _id = monthCost.real.id
+            if (monthCost.real.value && monthCost.real.value > 0) {
+                _id = monthCost.real.id || 0
                 _salary = monthCost.real.value || 0
                 _charges = monthCost.real.charges || 0
                 _total = monthCost.real.value + monthCost.real.charges || 0
