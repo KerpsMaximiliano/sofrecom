4ace351ae4a4ba97459e6ebb1bfbb19de3740341
Lucas Drughieri <ledrughieri@sofrecom.com.ar>
2020-01-17 09:51:34 -0300

    fix

--- src/Sofco.WebJob/Services/JobService.cs
+++ src/Sofco.WebJob/Services/JobService.cs
@@ -72,9 +72,9 @@ namespace Sofco.WebJob.Services
 
             RecurringJob.AddOrUpdate<IEmployeeSyncProfileJobService>(JobNames.EmployeeProfileHistory, j => j.Sync(true), Cron.Daily(8, 0), localTimeZone);
 
-            //RecurringJob.AddOrUpdate<IApplicantInterviewNotificationBeforeService>(JobNames.ApplicantInterviewNotificationBefore, j => j.Execute(), Cron.Daily(8, 0), localTimeZone);
+            RecurringJob.AddOrUpdate<IApplicantInterviewNotificationBeforeService>(JobNames.ApplicantInterviewNotificationBefore, j => j.Execute(), Cron.Daily(8, 0), localTimeZone);
 
-            //RecurringJob.AddOrUpdate<IApplicantInterviewNotificationAfterService>(JobNames.ApplicantInterviewNotificationAfter, j => j.Execute(), Cron.Daily(8, 0), localTimeZone);
+            RecurringJob.AddOrUpdate<IApplicantInterviewNotificationAfterService>(JobNames.ApplicantInterviewNotificationAfter, j => j.Execute(), Cron.Daily(8, 0), localTimeZone);
 
             RecurringJob.AddOrUpdate<ISocialChargesJob>(JobNames.SocialCharges, j => j.Execute(), Cron.Monthly(DateTime.DaysInMonth(DateTime.UtcNow.Year, DateTime.UtcNow.Month), 8), localTimeZone);
 
