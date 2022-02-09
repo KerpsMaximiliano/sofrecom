aa4573855606819ad17cb6a6d726d9971fe31742
David Comin <comin.david@outlook.com>
2022-02-09 11:51:48 -0300

    Front configuración
    Agregado de inputs, selección de usuarios, carga de datos y envío de los mismo.

--- src/Sofco.ClientWeb/src/app/views/admin/settings/settings.component.ts
+++ src/Sofco.ClientWeb/src/app/views/admin/settings/settings.component.ts
@@ -4,6 +4,8 @@ import { Subscription } from 'rxjs';
 import { SettingsService } from '../../../services/admin/settings.service';
 import { AppSettingService } from '../../../services/common/app-setting.service';
 import { AppSetting } from '../../../services/common/app-setting';
+import { UserService } from 'app/services/admin/user.service';
+import { forEach } from 'lodash';
 
 @Component({
   selector: 'app-settings',
@@ -21,21 +23,35 @@ export class SettingsComponent implements OnInit, OnDestroy {
   licenseTypes: Array<any> = new Array();
   jobSettings: Array<any> = new Array();
 
+  settingUsers: number[] = [];
+  usuarios: any[] = new Array<any>();
+
   constructor(
       private service: SettingsService,
       private messageService: MessageService,
       private appSettting: AppSetting,
-      private appSetttingService: AppSettingService) {
+      private appSetttingService: AppSettingService,
+      private userService: UserService) {
   }
 
   ngOnInit() {
     this.getAll();
     this.getLicenseTypes();
+    this.getUsers();
   }
 
   ngOnDestroy() {
     if (this.serviceSubscrip) { this.serviceSubscrip.unsubscribe(); }
     if (this.licenseTypesSubscrip) { this.licenseTypesSubscrip.unsubscribe(); }
+  }
+
+  getUsers() {
+    this.messageService.showLoading();
+    this.userService.getAll().subscribe(d=>{
+      d.forEach(user => {
+        this.usuarios.push({id:user.id, name: user.name})});
+      this.usuarios = [...this.usuarios]
+      this.messageService.closeLoading()});
   }
 
   getAll() {
@@ -45,6 +61,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
         const settings: Array<any> = d.body.data;
         this.settings = settings.filter(item => item.category !== this.JobsSettingCategory);
         this.jobSettings = settings.filter(item => item.category === this.JobsSettingCategory);
+        this.settingUsers = JSON.parse(this.jobSettings[2].value);
         this.loading = false
       },
       err => this.loading = false);
@@ -95,5 +112,23 @@ export class SettingsComponent implements OnInit, OnDestroy {
     this.licenseTypesSubscrip = this.service.saveItem(item).subscribe(
       response => { this.messageService.closeLoading(); this.messageService.succes('ADMIN.settings.saveSuccess'); },
       err => this.messageService.closeLoading());
+  }
+
+  saveSetting2(item) {
+    this.service.put({ key: "JobRecursosSinAsignacionDia", value: item}).subscribe(
+      response => { 
+        this.messageService.closeLoading();
+        this.messageService.succes('ADMIN.settings.saveSuccess');
+    },
+    err => this.messageService.closeLoading());
+  } 
+
+  saveSetting3(item) {
+    this.service.put({ key: "JobRecursosSinAsignacionDestinatarios", value: JSON.stringify(item)}).subscribe(
+      response => { 
+        this.messageService.closeLoading();
+        this.messageService.succes('ADMIN.settings.saveSuccess');
+    },
+    err => this.messageService.closeLoading());
   }
 }
