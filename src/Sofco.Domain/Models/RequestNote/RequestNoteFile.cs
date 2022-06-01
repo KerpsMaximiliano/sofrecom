3711057972a00ffaeaa8c112804857f2075f8223
Avaco Sistemas <avacosistemas@gmail.com>
2022-05-31 21:02:32 -0300

    RequestNote

---
+++ src/Sofco.Domain/Models/RequestNote/RequestNoteFile.cs
@@ -0,0 +1,12 @@
+﻿using Sofco.Domain.Models.Common;
+
+namespace Sofco.Domain.Models.RequestNote
+{
+    public class RequestNoteFile
+    {
+        public int FileId { get; set; }
+        public File File { get; set; }
+        public int RequestNoteId { get; set; }
+        public RequestNote RequestNote { get; set; }
+    }
+}
---
+++ src/Sofco.Domain/Models/RequestNote/RequestNoteTravelEmployee.cs
@@ -0,0 +1,15 @@
+﻿using Sofco.Domain.Models.AllocationManagement;
+using System;
+
+
+namespace Sofco.Domain.Models.RequestNote
+{
+    public class RequestNoteTravelEmployee : BaseEntity
+    {
+        public int RequestNoteTravelId { get; set; }
+        public RequestNoteTravel RequestNoteTravel { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
+
