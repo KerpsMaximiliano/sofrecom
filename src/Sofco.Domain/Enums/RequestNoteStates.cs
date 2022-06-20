6c159a8bff8fbe211e943e00ceffda070eff8ff9
IGNACIODIGIOIA <ignacio.digioia@gmail.com>
2022-06-20 14:51:17 -0300

    generacion de tabala de estados

---
+++ src/Sofco.Domain/Enums/RequestNoteStates.cs
@@ -0,0 +1,17 @@
+﻿namespace Sofco.Domain.RequestNoteStates
+{
+    public enum RequestNoteStates
+    {
+        Borrador = 1,
+        PendienteRevisiónAbastecimiento = 2,
+        PendienteAprobaciónGerentesAnalítica = 3,
+        PendienteAprobaciónAbastecimiento = 4,
+        PendienteAprobaciónDAF = 5,
+        Aprobada = 6,
+        SolicitadaAProveedor = 7,
+        RecibidoConforme = 8,
+        FacturaPendienteAprobaciónGerente=9,
+        PendienteProcesarGAF= 10,
+        Reachazada=11
+    }
+}
