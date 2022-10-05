2891d49ba8068501542ea4d2a106798382bdbfd7
David Comin <comin.david@outlook.com>
2022-10-05 18:00:52 -0300

    PurchaseOrdersController

---
+++ src/Sofco.Domain/Models/PurchaseOrders/PurchaseOrderItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofco.Domain.Models.PurchaseOrders
+{
+    public class PurchaseOrderItems
+    {
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
