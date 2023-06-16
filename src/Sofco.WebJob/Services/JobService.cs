7097381e9173d74067f257248bc34e8744ae76af
Avaco Sistemas <avacosistemas@gmail.com>
2023-06-15 23:47:27 -0300

    ultima configuracion jobs horas automaticas

--- src/Sofco.WebJob/Services/JobService.cs
+++ src/Sofco.WebJob/Services/JobService.cs
@@ -85,9 +85,9 @@ namespace Sofco.WebJob.Services
 
             RecurringJob.AddOrUpdate<IEmployeeTestPeriodJobService>(JobNames.EmployeeTestPeriod, j => j.Execute(), Cron.Daily(8), localTimeZone);
 
-            RecurringJob.AddOrUpdate<IEmployeeWorkTimesAddJob>(JobNames.EmployeeWorkTimesAdd, j => j.Execute(), Cron.Weekly(DayOfWeek.Thursday, 23, 30), localTimeZone);
+            RecurringJob.AddOrUpdate<IEmployeeWorkTimesAddJob>(JobNames.EmployeeWorkTimesAdd, j => j.Execute(), Cron.Weekly(DayOfWeek.Friday, 21), localTimeZone);
 
-            RecurringJob.AddOrUpdate<IEmployeeWorkTimesMonthAddJob>(JobNames.EmployeeWorkTimesMonthAdd, j => j.Execute(), Cron.Daily(23, 42), localTimeZone);
+            RecurringJob.AddOrUpdate<IEmployeeWorkTimesMonthAddJob>(JobNames.EmployeeWorkTimesMonthAdd, j => j.Execute(), Cron.Daily(23), localTimeZone);
 
 
         }
