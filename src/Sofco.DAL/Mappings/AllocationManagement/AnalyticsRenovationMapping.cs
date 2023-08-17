ffaedaedb9fcfbe0568efbedf4e30d3e0c88e794
Emmanuel Mansilla <lmansilla@sofredigital.com.ar>
2023-08-17 11:35:48 -0300

    add: agregado de AnalyticsRenovation completo, agregados de campos a Analytics #14273

---
+++ src/Sofco.DAL/Mappings/AllocationManagement/AnalyticsRenovationMapping.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sofco.Domain.Models.AllocationManagement;
+using Sofco.Domain.Models.Billing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofco.DAL.Mappings.AllocationManagement
+{
+    public static class AnalyticsRenovationMapping
+    {
+        public static void MapAnalyticsRenovation(this ModelBuilder builder)
+        {
+            builder.Entity<AnalyticsRenovation>().HasKey(x => x.Id);
+            builder.Entity<AnalyticsRenovation>().Property(x => x.Renovation);
+            builder.Entity<AnalyticsRenovation>().Property(x => x.AnalyticId);
+            builder.Entity<AnalyticsRenovation>().Property(x => x.Orden);
+            builder.Entity<AnalyticsRenovation>().Property(x => x.OpportunityId);
+            builder.Entity<AnalyticsRenovation>().Property(x => x.ModifiedBy);
+            builder.Entity<AnalyticsRenovation>().Property(x => x.CreatedBy);
+
+            builder.Entity<AnalyticsRenovation>().HasOne(x => x.Analytic).WithMany(x => x.AnalyticsRenovations).HasForeignKey(x => x.AnalyticId);
+            builder.Entity<AnalyticsRenovation>().HasOne(x => x.Opportunity).WithMany(x => x.AnalyticsRenovations).HasForeignKey(x => x.OpportunityId);
+        }
+    }
+}
